079635e09719c0702e3f8c04dc7bb7df408c065f
Waseem Q <waseemq@tracksimple.com>
2011-04-21 20:24:15 -0700

    Actually

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AwesomeQ::Application.config.secret_token = '68225cb8e3b0ad3aecf2d1c244f01a0354d8ec66c83cfb492de78c54836e6f862e1c6eed8922c9b3ec004b914cfb7d1bd67f75e0af08bb5bbd961a48c2916391'
